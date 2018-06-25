9f3cc5177ec14cf60c051d739db8c1987a056c70
lixuefeng-MBP <Calchas@vip.qq.com>
2018-06-25 23:33:06 +0800

    create new

--- public/js/main.js
+++ public/js/main.js
@@ -8,4 +8,26 @@ $(function(){
             $(".g-teacher-recommend .u-content .u-btn:eq("+i+")").stop().removeClass("u-btn-light");
         });
     });
+
+
+    var $centerwell_first = $('#centerwell .s:first');
+    $centerwell_first.animate({ width: '640px' }, "fast");
+    $centerwell_first.find('h3').addClass("on");
+
+    $('#centerwell .s').mouseenter(function(j) {
+        $("#centerwell .s .mask").show();
+        $(this).children('#centerwell .mask').hide();
+        $(this).stop(true,true).animate({ width: '640px' },'2000').siblings().stop(true,true).animate({ width: '120px' },'2000');
+    });
+
+
+    var $centerwell_first = $('.g-teacher-box .s:first');
+    $centerwell_first.animate({ width: '640px' }, "fast");
+    $centerwell_first.find('h3').addClass("on");
+
+    $('.g-teacher-box .s').mouseenter(function(j) {
+        $(".g-teacher-box .s .mask").show();
+        $(this).children('#centerwell .mask').hide();
+        $(this).stop(true,true).animate({ width: '640px' },'2000').siblings().stop(true,true).animate({ width: '120px' },'2000');
+    });
 });
