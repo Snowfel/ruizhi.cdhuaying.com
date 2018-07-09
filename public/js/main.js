e8ca8e56b141f324f08615d273fb01e375c095f2
Calchas <calchas@vip.qq.com>
2018-07-09 18:31:41 +0800

    update files

--- public/js/main.js
+++ public/js/main.js
@@ -21,6 +21,21 @@ $(function(){
         orderInfoPost(arrOrderInfo);
     });
 
+    /*锚点平滑滚动*/
+    $('a[href*=#],area[href*=#]').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var $target = $(this.hash);
+            $target = $target.length && $target || $('[name=' + this.hash.slice(1) + ']');
+            if ($target.length) {
+                var targetOffset = $target.offset().top;
+                $('html,body').animate({
+                        scrollTop: targetOffset
+                    },
+                    1000);
+                return false;
+            }
+        }
+    });
 });
 
 /**
