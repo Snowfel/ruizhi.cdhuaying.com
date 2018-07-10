330eb2123b29db4c75d32a14f882328957c3f028
Calchas <calchas@vip.qq.com>
2018-07-10 09:06:47 +0800

    update files

--- public/js/main.js
+++ public/js/main.js
@@ -22,7 +22,7 @@ $(function(){
     });
 
     /*锚点平滑滚动*/
-    $('a[href*=#],area[href*=#]').click(function() {
+    $('.a-name-smooth').click(function() {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var $target = $(this.hash);
             $target = $target.length && $target || $('[name=' + this.hash.slice(1) + ']');
